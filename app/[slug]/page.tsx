9a8bd36d40f17829d99219e8a01a36ead1d22bff
yokyaa <arjenrobbennn@gmail.com>
2023-07-08 18:38:26 +0300

    starting product details page

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import EditorOutput from '@/components/EditorOutput';
+import FeaturedProductCard from '@/components/FeaturedProductCard';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>editor output</div>;
+};
+
+export default page;
