2775e84fc4ea28ca43aa467629654233da392ae4
yokyaa <arjenrobbennn@gmail.com>
2023-07-14 15:24:47 +0300

    footer added dot icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Dot } from 'lucide-react';
 import Link from 'next/link';
 import { FC } from 'react';
 import { Separator } from './ui/Seperator';
@@ -6,14 +7,25 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = ({}) => {
   return (
     <>
-      <Separator className="bg-borderShinyblue my-5 " />
+      <Separator className="bg-borderShinyblue my-5 container" />
       <div className="flex justify-between text-lightSlateGray container text-sm pb-2">
         <div className="">Copyright © 2023 - Pro Review Product</div>
         <div className="flex gap-3">
-          <Link href="https://twitter.com">Twitter</Link>
-          <Link href="https://instagram.com">Instagram</Link>
-          <Link href="https://github.com">Github</Link>
-          <Link href="https://linkedin.com">LinkedIn</Link>
+          <div className="flex">
+            <Link href="https://twitter.com">Twitter</Link>
+            <Dot className="w-4" />
+          </div>
+          <div className="flex">
+            <Link href="https://instagram.com">Instagram</Link>
+            <Dot className="w-4 " />
+          </div>
+          <div className="flex">
+            <Link href="https://github.com">Github</Link>
+            <Dot className="w-4 " />
+          </div>
+          <div className="flex">
+            <Link href="https://linkedin.com">LinkedIn</Link>
+          </div>
         </div>
       </div>
     </>
