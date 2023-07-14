04636e8cfafa5eccc7564354c4eaea19f647f1f6
yokyaa <arjenrobbennn@gmail.com>
2023-07-14 16:28:08 +0300

    added social media links to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,10 @@
+import { Github, Instagram, Linkedin, Minus, Twitter } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const page = () => {
   return (
-    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 pt-8">
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-10 pt-8">
       <div className="flex flex-col space-y-2 justify-center items-center text-center">
         <div className="relative w-36 h-36">
           <Image
@@ -22,6 +24,31 @@ const page = () => {
           Discover and decide on the top products to buy, and be the first to
           access the best daily offerings.
         </p>
+        <div className="flex pt-20 text-lightSlateGray">
+          <div className="flex">
+            <Link className="text-lg" href="https://twitter.com">
+              <Twitter className="w-10" />
+            </Link>
+            <Minus className="w-6" />
+          </div>
+          <div className="flex">
+            <Link className="flex" href="https://instagram.com">
+              <Instagram className="w-10" />
+            </Link>
+            <Minus className="w-6" />
+          </div>
+          <div className="flex">
+            <Link className="flex" href="https://linkedin.com">
+              <Linkedin className="w-10" />
+            </Link>
+            <Minus className="w-6" />
+          </div>
+          <div className="flex">
+            <Link className="flex" href="https://github.com">
+              <Github className="w-10" />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
